194756d74fdbacf2fb74a08c227474f377775023
phil <philgriffin@gmail.com>
2017-05-13 17:21:09 +0100

    test

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(GetMessage());
+        }
+
+        public static string GetMessage()
+        {
+            return "hello world";
+        }
+
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var hw = new HelloWorld();
+            Assert.AreEqual("hello world", HelloWorld.GetMessage());
+
+        }
+    }
+}
