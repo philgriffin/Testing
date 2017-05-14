67e15198b331ffb6fda1afdde2deecb573bc9d3b
phil <philgriffin@gmail.com>
2017-05-14 15:49:00 +0100

    added logging package

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 namespace ConsoleApplication
 {
+    
     public class HelloWorld
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+         (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    
         public static void Main(string[] args)
         {
             Console.WriteLine(GetMessage());
@@ -15,6 +20,7 @@ namespace ConsoleApplication
 
         public static string GetMessage()
         {
+            log.Info("test");
             return "hello world";
         }
 
